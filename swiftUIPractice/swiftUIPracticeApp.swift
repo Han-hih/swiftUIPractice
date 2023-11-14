8fbafd4b2501a76843bd3faee7fb12633faaea7d
한 <hih458@naver.com>
2023-11-14 16:33:07 +0900

    Initial Commit

---
+++ swiftUIPractice/swiftUIPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUIPracticeApp.swift
+//  swiftUIPractice
+//
+//  Created by 황인호 on 11/14/23.
+//
+
+import SwiftUI
+
+@main
+struct swiftUIPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
